70c51be5749d193d570f6d0529a4756423f5758e
Raluca Grigoras <ralucav@gmail.com>
2020-10-12 17:29:46 +0300

    Add readme, remove unused files

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Note;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Session;
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    redirect(URL::to('/notes')); // TODO
+    return redirect('notes');
 });
 Route::resource('notes', \App\Http\Controllers\NoteController::class);
