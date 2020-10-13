e0beba6267ff8afa04cfdf86d2f397acf8aea651
Raluca Grigoras <ralucav@gmail.com>
2020-10-13 15:30:43 +0300

    update db connection

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Str;
 
-$DATABASE_URL=parse_url(‘DATABASE_URL’);
+$DATABASE_URL=parse_url(‘postgres://yotvajbwfmvjus:362b8dab7e26696726aa7d5449235efa60db71f5d53f4e96683e4cde907d8564@ec2-3-215-207-12.compute-1.amazonaws.com:5432/ddtaefv8tisca1’);
 
 return [
 
@@ -67,17 +67,17 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL['host'],
+            'port' => $DATABASE_URL['port'],
+            'database' => ltrim($DATABASE_URL['path'], "/"),
+            'username' => $DATABASE_URL['user'],
+            'password' => $DATABASE_URL['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
+            'url' => $DATABASE_URL,
         ],
 
         'sqlsrv' => [
